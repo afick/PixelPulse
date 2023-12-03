64156396b741f7f12cb357e0512d7a912f8c762f
afick <Alexander.W.Fick.25@dartmouth.edu>
2023-12-03 14:21:23 +0100

    adding postitem dataclasses

---
+++ app/src/main/java/hu/ait/pixelpulse/data/PostItem.kt
@@ -0,0 +1,39 @@
+package hu.ait.pixelpulse.data
+
+data class Post(
+    var uid: String = "",
+    var author: String = "",
+    var title: String = "",
+    var body: String = "",
+    var imgUrl: String = "",
+    var imgMetadata: ImgMetadata
+)
+
+data class ImgMetadata(
+    var uid: String = "",
+    var camera: Camera,
+    var lens: Lens,
+    var shutterSpeed: String = "",
+    var aperture: String = "",
+    var iso: String = "",
+)
+
+data class Camera(
+    var uid: String = "",
+    var name: String = "",
+    var brand: String = "",
+    var model: String = "",
+    var imgUrl: String = ""
+)
+
+data class Lens(
+    var uid: String = "",
+    var brand: String = "",
+    var model: String = "",
+    var focalLength: String = "",
+    var aperture: String = ""
+)
+data class PostWithId(
+    val postId: String,
+    val post: Post
+)
--- app/src/main/java/hu/ait/pixelpulse/ui/screen/auth/login/LoginScreen.kt
+++
@@ -1,2 +0,0 @@
-package hu.ait.pixelpulse.ui.screen.auth.login
-
